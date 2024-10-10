e18262b28e6eb1a38fac6451167c46f1560fe650
Nina Høegh-Larsen <ninius@online.no>
2024-10-10 09:52:05 +0200

    Day 2 Part 1 Complete

---
+++ src/02.py
@@ -0,0 +1,47 @@
+# AoC 2018 Day 2
+
+file = "../input/02.txt"
+
+with open(file, "r") as f:
+    raw = f.readlines()
+    f.close()
+
+def parse(raw):
+    data = []
+    
+    for line in raw:
+        line = line.strip()
+        data.append(line)
+
+    return data
+
+data = parse(raw)
+
+def has_two(line):
+
+    for letter in line:
+        if (line.count(letter) == 2):
+            return True
+
+    return False
+
+def has_three(line):
+
+    for letter in line:
+        if (line.count(letter) == 3):
+            return True
+
+    return False
+
+# Part 1
+
+two = 0
+three = 0
+
+for line in data:
+    if has_two(line):
+        two += 1    
+    if has_three(line):
+        three += 1
+
+print("The checksum is", two*three)
